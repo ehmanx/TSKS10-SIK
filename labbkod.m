6970619671bb4208592450f63b716259b98c831d
Gustav <ehmanx@gmail.com>
2018-05-13 14:58:09 +0200

    fixed one figure and changed some code

--- labbkod.m
+++ labbkod.m
@@ -21,11 +21,11 @@ f_axel = (0:signal_l/2-1)*sampel_frek/signal_l;
 t_axel = linspace(0,signal_l/sampel_frek, signal_l);
 
 % Plotta |Y(f)| för att undersöka signalen i frekvsensspektrumet
-% figure;
-% plot(f_axel, abs(fft_signal));
-% title('Amplitud spektrum');
-% xlabel('Frekvens f [kHz]');
-% ylabel('Amplitud');
+figure;
+plot(f_axel, abs(fft_signal));
+title('Amplitud spektrum');
+xlabel('Frekvens f [kHz]');
+ylabel('Amplitud');
 
 
 % Bandbredden är 20 kHz
@@ -37,40 +37,40 @@ frek_2 = 74000; % 74 kHz
 frek_3 = 112000; % 112 kHz
 
 %skapa ny figur
-%figure;
+figure;
 
 % Bandpassfilter för att undersöka första möjliga fc, 36 kHz 
 [B1,A1] = butter(10, [30000,50000]/(sampel_frek/2));
 
 delsignal1 = filter(B1, A1, signal);
 
-% subplot(3,1,1);
-% plot(t_axel, delsignal1);
-% xlabel('Tid [s]');
-% ylabel('Amplitud');
-% title('Bärfrekvens 37 kHz');
+subplot(3,1,1);
+plot(t_axel, delsignal1);
+xlabel('Tid [s]');
+ylabel('Amplitud');
+title('Bärfrekvens 36 kHz');
 
 % Bandpassfilter för att undersöka andra möjliga fc, 74 kHz 
 [B2,A2] = butter(10, [60000,80000]/(sampel_frek/2));
 
 delsignal2 = filter(B2, A2, signal);
 
-% subplot(3,1,2);
-% plot(t_axel, delsignal2);
-% xlabel('Tid [s]');
-% ylabel('Amplitud');
-% title('Bärfrekvens 74 kHz');
+subplot(3,1,2);
+plot(t_axel, delsignal2);
+xlabel('Tid [s]');
+ylabel('Amplitud');
+title('Bärfrekvens 74 kHz');
 
 % Bandpassfilter för att undersöka tredje möjliga fc, 112 kHz 
 [B3,A3] = butter(10, [110000,120000]/(sampel_frek/2));
 
 delsignal3 = filter(B3, A3, signal);
 
-% subplot(3,1,3);
-% plot(t_axel, delsignal3);
-% xlabel('Tid [s]');
-% ylabel('Amplitud');
-% title('Bärfrekvens 112 kHz');
+subplot(3,1,3);
+plot(t_axel, delsignal3);
+xlabel('Tid [s]');
+ylabel('Amplitud');
+title('Bärfrekvens 112 kHz');
 
 % Bandpassfilter som används för att bestämma f1,f2
 [B4,A4] = butter(10, [100000,103000]/(sampel_frek/2));
@@ -84,8 +84,8 @@ fft_del4 = fft_del4(1:del_l/2);
 
 f_axel_ = sampel_frek/2*linspace(0, 1, del_l/2);
 
-% figure;
-% plot(f_axel, abs(fft_del4));
+figure;
+plot(f_axel, abs(fft_del4));
 
 fc = 112000; % Från |Y(t)| 
 f1 = 102500; % Från |Y(t)|
@@ -98,10 +98,10 @@ f2 = 102501; % Från |Y(t)|
 lags = lags(lags>0);
 korrelation = korrelation(lags>0);
 
-% figure;
-% plot(lags/sampel_frek, abs(korrelation));
-% xlabel('Tid [s]');
-% title('Korrelation');
+figure;
+plot(lags/sampel_frek, abs(korrelation));
+xlabel('Tid [s]');
+title('Korrelation');
 
 % Från figur
 fordojning = 0.39;
@@ -128,7 +128,7 @@ end
 [B5,A5] = butter(10,bandbredd/(sampel_frek/2),'low');
 
 % Fasvridningskonstant
-phi = pi/4;
+phi = 0.8; %pi/4 < phi < pi/3 
 
 for n = 0: 0.1:pi/2
     disp(n)
@@ -144,7 +144,7 @@ for n = 0: 0.1:pi/2
     q = downsample(X_q, 10);
     
     %Uppspelning
-    soundsc(i, sampel_frek/10);
+    %soundsc(i, sampel_frek/10);
     %soundsc(q, sampel_frek/10);
     pause;
 end
